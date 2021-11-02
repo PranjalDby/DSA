fe99358e0dbb25f65543b7a8086c1bef57657bff
pranjaldgit <dpranjal.088@gmail.com>
2021-11-02 20:12:39 +0530

    new

--- arrayinsertion.c
+++ arrayinsertion.c
@@ -1,32 +1,20 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+void display(int arr[],int size)
+{
+    printf("elements in array is:\n");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d\n",arr[i]);
+    }
+    
+}
 int main()
 {
-int arr[5]={1,2,4,5};
-int pos,elements,p;
-int i;
-   elements=5;
-   for(i=0;i<elements;i++)
-   {
-       printf("%d\n",arr[i]);
-   }
-    printf("enter the pos:\n");
-   scanf("%d",&pos);
-    p=33;
-   for(i=elements-1;i>=pos;i--)
-   {
-       arr[i+1]=arr[i];
-       arr[pos]=p;
-   }
-   elements=elements+1;
-   printf("array ad-fetr inser:\n");
-   for ( i = 0; i <elements; i++)
-   {
-       printf("%d\n",arr[i]);
-   }
-   
-    
+    int arr[100]={1,2,3,4};
+    int size=4;
+    display(arr,size);    
  
    
     
