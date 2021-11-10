d360bd6e188c6b787692e553ab713c5f170fa1db
pranjaldgit <dpranjal.088@gmail.com>
2021-11-10 17:08:36 +0530

    enter

--- 2darray.c
+++ 2darray.c
@@ -1,6 +1,20 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+void traverse(int arr[10][10],int rows,int cols)
+{
+    printf("enter the rows and cols:\n");
+    scanf("%d%d",&rows,&cols);
+    printf("enter the elements:\n");
+    int i,j;
+    for(i=0;i<rows;i++)
+    {
+        for(j=0;j<cols;j++)
+        {
+            scanf("%d",&arr[i][j]);
+        }
+    }
+}
 int main()
 {/* 
     int arr[2][4];
@@ -14,5 +28,33 @@ int main()
         }
     }
      */
-    
+   /*  int arr[5]={2,4,6,8,9};
+    int *ptr;
+    ptr=&arr[0];
+    for(int i=0;i<5;i++)
+    {
+    printf("%d\n",*(ptr+i));
+    } */
+
+    // formation of pointer array
+    // int *ptr[4],p=1,q=4,r=8,s=10;
+   /*  ptr[0]=&p;
+    ptr[1]=&q;
+    ptr[2]=&r;
+    ptr[3]=&s;
+    for(int i=0;i<4;i++)
+    {
+        printf("%d\n",*(ptr[i]));
+    }
+ */
+/* int arr1[]={1,2,3,4},arr2[]={6,8,10,12};
+    int *ptrs[2];
+    ptrs[0]=&arr1[2];
+    ptrs[1]=&arr2[3];
+    for (int i = 0; i < 2; i++)
+    {
+        printf("%d\n",*(ptrs[i]));
+    } */
+    int arr[10][10],rows,cols;
+    traverse(arr,rows,cols);
 }
