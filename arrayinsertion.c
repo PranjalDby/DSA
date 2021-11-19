c39808b7c39bddc0d7ec569e6628314eec3c3473
pranjaldgit <dpranjal.088@gmail.com>
2021-11-19 08:37:26 +0530

    update arrayinsertion.c

--- arrayinsertion.c
+++ arrayinsertion.c
@@ -1,40 +1,49 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-void display(int arr[],int size)
+void insert(int arr[10],int size)
 {
-    printf("elements in array is:\n");
-    for (int i = 0; i < size; i++)
+    int i,pos,element;
+    printf("enter the element of an array;\n");
+     for(i=0;i<size;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    printf("enter the pos of an array:\n");
+    scanf("%d",&pos);
+   /*  printf("enter the element:\n");
+    scanf("%d",&element); */
+    printf("\n");
+   /*  for(i=size-1;i>=pos;i--)
+    {
+        if(i>=pos)
+        {
+            arr[i+1]= arr[i];
+        }
+         arr[pos]=element;
+    }
+ */
+    for(i=pos;i<size-1;i++)
+    {
+       arr[i]=arr[i+1];
+    }
+}
+void display(int arr[10],int size)
+{   
+    int i;
+    for(i=0;i<size;i++)
     {
         printf("%d\n",arr[i]);
     }
-    
-}
-void insert(int arr[],int size,int element,int index,int max)
-{
-    if(size>=max){
-        printf("not enough space:\n");
-    }
-    int i=size;
-    while (i>=index)
-    {
-        arr[i+1]=arr[i];
-        i--;
-    }
-    arr[index]=element;
-    
 }
 int main()
 {
-    int arr[100]={1,2,3,4};
-    int size=4;
-    int index=3;
-    int element=6;
-    insert(arr,size,element,index,100);
-    size=size+1;
-    display(arr,size);    
- 
-   
-    
-    
+    int arr[10];
+    int s;
+    printf("enter the size of an array;\n");
+    scanf("%d",&s);
+    insert(arr,s);
+   /*  s=s+1; */
+   s=s-1;
+    display(arr,s);
 }
