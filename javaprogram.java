a1c58457450b4aa98a4c96251467576ec610d572
pranjal <dpranjal.088@gmail.com>
2022-01-02 16:39:19 +0530

    j

---
+++ javaprogram.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class javaprogram {
+    public static void main(String[] args) {
+        int x=44;
+        int z=43;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the value:");
+        int val=sc.nextInt();
+        System.out.println(x+val);
+    }
+}
