e17ebd715a83093840a10253688812679c4c85f1
pranjal <dpranjal.088@gmail.com>
2022-01-10 18:49:37 +0530

    10 jan_2022

---
+++ javaprgrm.java
@@ -0,0 +1,8 @@
+public class javaprgrm {
+   public static void main(String[] args) {
+       int a=44;
+       Float fld=4.5f;
+       System.out.println("int \t" + a );
+       System.out.println("dloat \t" + fld );
+   }
+}
