c520a240a3bf82f3f43764a06e72a5ca5ee19255
Pranjal <dpranjal.088@gmail.com>
2022-01-24 19:23:30 +0530

    jan_end_changes

---
+++ java_jan.java
@@ -0,0 +1,20 @@
+
+public class java_jan
+{
+  public static void main(String[] args) {
+      System.out.println("hello world\n");
+      System.out.println("data types in java are:\n");
+      int num=34;
+      float y=3.4f;
+      char x='a';
+      String name;
+      name="aryan";
+      double n=3.2222233444;
+      System.out.println("Num Is"+num);
+      System.out.println("float Num Is"+y);
+      System.out.println("char Is"+x);
+      System.out.println("string Is"+name);
+      System.out.println("boolean Is"+ n);
+      
+  }
+}
